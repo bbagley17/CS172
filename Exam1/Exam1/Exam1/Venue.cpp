2ab5d575dba34efc88a09da742a194872a1543cc
bbagley17 <bbagley17@my.whitworth.edu>
2016-03-03 10:53:04 -0800

    Exam1 Draft 2
    Added the honor system statement to the top of one file which was
    previously missing it.

--- Exam1/Exam1/Exam1/Venue.cpp
+++ Exam1/Exam1/Exam1/Venue.cpp
@@ -1,4 +1,5 @@
-
+/* I affirm that all code given below was written solely by me, Bryce Bagley,
+and that any help I received adhered to the rules stated for this exam. */
 #include <iostream>
 #include <string>
 #include "Event.h"
